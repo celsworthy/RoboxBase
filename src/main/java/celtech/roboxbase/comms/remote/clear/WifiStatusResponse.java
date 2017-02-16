1a8fe1290f4392bc91fadbe7ab58a928cd289bc9
Ian Hudson <ian@cel-uk.com>
2017-02-16 09:54:12 +0000

    Added wifi response structure

---
+++ src/main/java/celtech/roboxbase/comms/remote/clear/WifiStatusResponse.java
@@ -0,0 +1,54 @@
+package celtech.roboxbase.comms.remote.clear;
+
+/**
+ *
+ * @author ianhudson
+ */
+public class WifiStatusResponse
+{
+    private boolean poweredOn;
+    private boolean associated;
+    private String ssid;
+
+    public WifiStatusResponse()
+    {
+        // Jackson deserialization
+    }
+
+    public WifiStatusResponse(boolean poweredOn, boolean associated, String ssid)
+    {
+        this.poweredOn = poweredOn;
+        this.associated = associated;
+        this.ssid = ssid;
+    }
+
+    public boolean isPoweredOn()
+    {
+        return poweredOn;
+    }
+
+    public void setPoweredOn(boolean poweredOn)
+    {
+        this.poweredOn = poweredOn;
+    }
+
+    public boolean isAssociated()
+    {
+        return associated;
+    }
+
+    public void setAssociated(boolean associated)
+    {
+        this.associated = associated;
+    }
+
+    public String getSsid()
+    {
+        return ssid;
+    }
+
+    public void setSsid(String ssid)
+    {
+        this.ssid = ssid;
+    }
+}
